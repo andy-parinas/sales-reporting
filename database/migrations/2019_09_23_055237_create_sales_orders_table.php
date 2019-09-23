6ff051c8ec3f937e080e947200cd8110077679d9
Andy Parinas <parinasadolfo@gmail.com>
2019-09-23 16:23:40 +1000

    Added resources for Agent Guide and SalesOrder

---
+++ database/migrations/2019_09_23_055237_create_sales_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('order_number');
+            $table->string('grp_code');
+            $table->string('pax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_orders');
+    }
+}
