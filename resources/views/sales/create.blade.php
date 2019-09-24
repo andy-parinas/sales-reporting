6cec5c9d3bdc1f792ca5fd56540dfa0c3c3b4dd2
Andy Parinas <parinasadolfo@gmail.com>
2019-09-24 19:39:47 +1000

    implemented salereport form ui

---
+++ resources/views/sales/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="flex content-center items-center justify-between flex-wrap bg-gray-200 h-24 border-b border-gray-300">
+        <div class="px-4 py-2">
+            <h1 class="text-4xl font-bold uppercase text-gray-800" >Create Sales Report</h1>
+        </div>
+    </div>
+   <sales-report-form></sales-report-form>
+   
+@endsection
