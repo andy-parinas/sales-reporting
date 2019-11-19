ffee502780cf9a60c988dc9cd3133788f300d1f0
Andy Parinas <parinasadolfo@gmail.com>
2019-11-19 16:46:03 +1000

    include TourCommission on TourAgent Creation

--- database/seeds/TourAgentSeeder.php
+++ database/seeds/TourAgentSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Commission;
+use App\CommissionType;
 use App\TourAgent;
+use App\TourCommission;
+use App\TourType;
 use Illuminate\Database\Seeder;
 
 class TourAgentSeeder extends Seeder
@@ -21,12 +25,35 @@ class TourAgentSeeder extends Seeder
 
 
         foreach ($tourAgents as $agent) {
-            factory(TourAgent::class)->create([
+            $tourAgent = factory(TourAgent::class)->create([
                 'name' => $agent,
                 'address' => null,
                 'email' => null,
                 'phone' => null
             ]);
-        }
+
+            
+            $tourTypes = TourType::all();
+            $commissionTypes = CommissionType::all();
+            $commissions = Commission::all();
+
+            foreach ($tourTypes as $tourType) {
+                
+                foreach ($commissionTypes as $commissionType) {
+                    
+                    foreach ($commissions as $commission) {
+                        
+                        factory(TourCommission::class)->create(([
+                            'tour_agent_id' => $tourAgent->id,
+                            'tour_type_id' => $tourType->id,
+                            'commission_type_id' => $commissionType->id,
+                            'commission_id' => $commission->id,
+                            'amount' => 0
+                        ]));
+
+                    }
+                }
+            }
+        } // End of TourAgents forEach
     }
 }
